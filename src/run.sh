fa11ee29e06a3b35f8049fb5d411eb47c009a206
vegter <amsjurien@gmail.com>
2018-07-02 13:15:18 +0200

    Empty db after clean Run per module

--- src/run.sh
+++ src/run.sh
@@ -4,10 +4,17 @@ if [ "$1" = "--clean" ]
 then
     echo Cleaning data
     cp ./reader/original_data/* ./reader/input_data
+    rm ./reader/output_data/*
     rm ./processor/output_data/*
     cp ./writer/original_data/* ./writer/data
     rm ./api/output_data/*
     rm ./export/output_data/*
+elif [ "$1" != "" ]
+then
+    MODULE=$1
+    echo Run $MODULE
+    cd $MODULE
+    python main.py
 else
     echo Run modules
     for module in "reader" "processor" "writer" "api" "export"
